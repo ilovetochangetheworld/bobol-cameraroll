68d1e85256381b7775c0eb3c2476e300071c56a3
yixin <364130259@qq.com>
2020-03-03 19:28:39 +0800

    init

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,7 @@ module.exports = {
       "module-resolver",
       {
         alias: {
-          "@react-native-community/cameraroll": "./js/CameraRoll.js"
+          "bobol-cameraroll": "./js/CameraRoll.js"
         },
         cwd: "babelrc"
       }
